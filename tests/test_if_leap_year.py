39feb8ab8926b6d22fd5e3a617f9f628794450cf
Karoline <kanojo94@gmail.com>
2023-10-25 22:53:56 +0200

    test with year that is not a leap year

--- tests/test_if_leap_year.py
+++ tests/test_if_leap_year.py
@@ -8,26 +8,26 @@ test_year_2 = years_that_are_not_leap_years[0]
 
 
 def test_year_is_divisible_by_4():
-    division = is_leap_year(test_year_1) % 4
-    assert division, f'\n{test_year_1} was not divisible by 4'
+    division = is_leap_year(test_year_2) % 4
+    assert division, f'\n{test_year_2} was not divisible by 4'
 
 
 def test_year_is_not_divisible_by_100():
-    division = is_leap_year(test_year_1) % 100
-    assert not division, f'\n{test_year_1} was divisible by 100'
+    division = is_leap_year(test_year_2) % 100
+    assert not division, f'\n{test_year_2} was divisible by 100'
 
 
 def test_year_is_divisible_by_400():
-    division = is_leap_year(test_year_1) % 400
-    assert division, f'\n{test_year_1} was not divisible by 400'
+    division = is_leap_year(test_year_2) % 400
+    assert division, f'\n{test_year_2} was not divisible by 400'
 
 
 def test_year_is_leap_year():
-    is_it_leap_year = is_leap_year(test_year_1)
-    assert is_it_leap_year, f'\n{test_year_1} is not a leap year'
+    is_it_leap_year = is_leap_year(test_year_2)
+    assert is_it_leap_year, f'\n{test_year_2} is not a leap year'
 
 
 def test_year_is_not_a_leap_year():
-    is_it_leap_year = is_leap_year(test_year_1)
-    assert not is_it_leap_year, f'\n{test_year_1} is in fact a leap year...'
+    is_it_leap_year = is_leap_year(test_year_2)
+    assert not is_it_leap_year, f'\n{test_year_2} is in fact a leap year...'
 
