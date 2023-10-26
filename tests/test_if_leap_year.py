4fdecb189efb4ed3469765f38089c00e99c5971c
Karoline <kanojo94@gmail.com>
2023-10-26 23:47:28 +0200

    edits

--- tests/test_if_leap_year.py
+++ tests/test_if_leap_year.py
@@ -1,44 +1,60 @@
 import pytest
-
 from src.leap_year import is_leap_year
 
-years_that_are_leap_years = [2000, 2016, 2020]
-years_that_are_not_leap_years = [2001, 1700, 1800, 1900, 2100]
+# Years marked with True are leap years
+test_leap_year = [
+    (1912, True),
+    (2000, True),
+    (2016, True),
+    (2020, True),
+    (2152, True),
+]
+
+test_not_leap_year = [
+    (2001, False),
+    (1700, False),
+    (1800, False),
+    (1900, False),
+    (2100, False),
+]
 
 
-@pytest.fixture(params=[1, 2])
-def test_year_choice(request):
-    return request.param
+# A leap year is divisible by 4 but not by 100
+@pytest.mark.parametrize("year, expected_result", test_leap_year)
+def test_year_is_divisible_by_4_but_not_100(year, expected_result):
+    result = (is_leap_year(year) % 4) and (is_leap_year(year) % 100 != 0)
+    assert result == expected_result, f'\n{year} is not divisible by 4 and is divisible by 100.'
 
 
-@pytest.fixture
-def test_year(test_year_choice):
-    if test_year_choice == 1:
-        return years_that_are_leap_years[2]
-    elif test_year_choice == 2:
-        return years_that_are_not_leap_years[3]
+# Therefore a leap year is divisible by 400
+@pytest.mark.parametrize("year, expected_result", test_leap_year)
+def test_year_is_divisible_by_400(year, expected_result):
+    result = (is_leap_year(year) % 400)
+    assert result == expected_result, f'\n{year} is divisible by 400.'
 
 
-def test_year_is_divisible_by_4(test_year):
-    division = is_leap_year(test_year) % 4
-    assert division, f'\n{test_year} was not divisible by 4'
+# A non-leap year is not divisible by 4
+@pytest.mark.parametrize("year, expected_result", test_not_leap_year)
+def test_year_is_not_divisible_by_4(year, expected_result):
+    result = is_leap_year(year) % 4
+    assert result == expected_result, f'\n{year} is not divisible by 4.'
 
 
-def test_year_is_not_divisible_by_100(test_year):
-    division = is_leap_year(test_year) % 100
-    assert not division == 0, f'\n{test_year} was divisible by 100'
+# A year is not a leap year if it is divisble by 100 but not by 400
+@pytest.mark.parametrize("year, expected_result", test_not_leap_year)
+def test_year_is_not_divisible_by_100_but_not_400(year, expected_result):
+    result = (is_leap_year(year) % 100 != 0) or (is_leap_year(year) % 400 == 0)
+    assert not result == expected_result, f'\n{year} is not divisible by 100 '
 
 
-def test_year_is_divisible_by_400(test_year):
-    division = is_leap_year(test_year) % 400
-    assert division, f'\n{test_year} was not divisible by 400'
+# General tests if years are leap years
+@pytest.mark.parametrize("year, expected_result", test_leap_year)
+def test_year_is_leap_year(year, expected_result):
+    result = is_leap_year(year)
+    assert result == expected_result, f'\n{year} is in fact a leap year...'
 
 
-def test_year_is_leap_year(test_year):
-    is_it_leap_year = is_leap_year(test_year)
-    assert is_it_leap_year, f'\n{test_year} is not a leap year'
-
-
-def test_year_is_not_a_leap_year(test_year):
-    is_it_leap_year = is_leap_year(test_year)
-    assert not is_it_leap_year, f'\n{test_year} is in fact a leap year...'
+@pytest.mark.parametrize("year, expected_result", test_not_leap_year)
+def test_year_is_leap_year(year, expected_result):
+    result = is_leap_year(year)
+    assert result == expected_result, f'\n{year} is in fact a leap year...'
